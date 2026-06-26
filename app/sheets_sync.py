021f021ce18dcc05b6340105cac58bcd11103339
sadaf953 <sadafmahvishedu@gmail.com>
2026-06-27 00:37:26 +0530

    fix sheets sync to use service account

--- app/sheets_sync.py
+++ app/sheets_sync.py
@@ -1,9 +1,17 @@
 import gspread
+import json
+import os
+from google.oauth2.service_account import Credentials
 
 def sync_to_payments_sheet(data):
-    client = gspread.oauth(
-        credentials_filename="client_secrets.json"
-    )
+    scopes = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    
+    creds_json = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+    creds = Credentials.from_service_account_info(creds_json, scopes=scopes)
+    client = gspread.authorize(creds)
     
     sheet = client.open("DocFlow").worksheet("Payments")
     
